fc5b5b62f6c6a2aff4ccaf7a6a8a37dd9a3e63aa
tsukitaku <takuma.tsukikawa@gmail.com>
2019-11-14 20:12:10 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 月川拓馬 on 2019/11/14.
+//  Copyright © 2019 tsukitaku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
